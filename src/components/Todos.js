34887b7d3a07ca6a71960dbe58763cb91e412b7f
razibmahmud50 <razibmahmud50@gmail.com>
2019-09-25 19:19:42 +0600

    react todos app

---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Todos  extends Component {
+  render(){
+    return (
+      <div>
+        <h2>To Dos</h2>
+      </div>
+    );
+  }
+  
+}
+
+export default Todos;
